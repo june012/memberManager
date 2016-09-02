0bbd309fd8d765b39d7de609db2879acb4d2802e
彭婉 <wan.peng@msxf.com>
2016-09-02 16:57:25 +0800

    删除终端服务 内容管理 OA办公

--- thinker/thinker-web/src/main/java/org/guess/showcase/finance/dao/FinanceDao.java
+++
@@ -1,8 +0,0 @@
-package org.guess.showcase.finance.dao;
-
-import org.guess.core.orm.EntityDao;
-import org.guess.showcase.finance.model.Finance;
-
-public interface FinanceDao extends EntityDao<Finance, Long>{
-	
-}
