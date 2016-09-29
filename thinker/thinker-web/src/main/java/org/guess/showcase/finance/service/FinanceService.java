3b6a7beb2c0d6d12cc7e1cae8a5ccc84832d36c4
彭婉 <wan.peng@msxf.com>
2016-09-29 16:36:14 +0800

    删除finance

--- thinker/thinker-web/src/main/java/org/guess/showcase/finance/service/FinanceService.java
+++
@@ -1,8 +0,0 @@
-package org.guess.showcase.finance.service;
-
-import org.guess.core.service.BaseService;
-import org.guess.showcase.finance.model.Finance;
-
-public interface FinanceService extends BaseService<Finance, Long>{
-
-}
